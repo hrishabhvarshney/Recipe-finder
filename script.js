d5b3a7cea99f5764f91d5528fb54b865a584c91a
Hrishabh Varshney <122474444+hrishabhvarshney@users.noreply.github.com>
2024-04-05 23:58:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("button").addEventListener('click',()=>{
     let inputValue = document.getElementById('inputName').value 
     let details = document.getElementById("details")
     details.innerHTML = ""
-    fetch(`https:www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
         .then(response => response.json())
         .then(data=> {
             const items = document.getElementById("items")
@@ -32,7 +32,7 @@ document.getElementById("button").addEventListener('click',()=>{
 })
 
 function details(id){
-    fetch(`https:www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     .then(res=>res.json())
     .then(detail => {
         let meal = detail.meals[0]
